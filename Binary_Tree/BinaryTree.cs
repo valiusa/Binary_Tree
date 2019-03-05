79eb1dfd238e2a14ec59e5c2bc8b83609ce63776
valiusa <valentinvladimirov@abv.bg>
2019-03-05 14:17:32 +0200

    binary tree

---
+++ Binary_Tree/BinaryTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    class BinaryTree
+    {
+        Node root;
+
+        BinaryTree(int key)
+        {
+            root = new Node(key);
+        }
+
+        BinaryTree()
+        {
+            root = null;
+        }
+
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+
+            tree.root = new Node(1);
+            tree.root.left = new Node(2);
+            tree.root.right = new Node(3);
+            tree.root.left.left = new Node(4);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Binary_Tree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Tree
+{
+    class Node
+    {
+        int key;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            key = item;
+            left = right = null;
+        }
+    }
+}
